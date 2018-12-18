6ccb56b73dd80c9d0547325fe1326d76584660b9
univ <fcsnwu@163.com>
2018-12-18 11:46:48 +0800

    [maven]插件的基础知识

---
+++ module-B/src/test/java/com/univ/BTest.java
@@ -0,0 +1,16 @@
+package com.univ;
+
+import org.junit.Test;
+
+/**
+ * @author univ
+ * @datetime 2018/12/17 9:26 PM
+ * @description
+ */
+public class BTest {
+    
+    @Test
+    public void test() {
+        System.out.println("hello");
+    }
+}
---
+++ module-web/src/test/java/com/univ/WebTest.java
@@ -0,0 +1,16 @@
+package com.univ;
+
+import org.junit.Test;
+
+/**
+ * @author univ
+ * @datetime 2018/12/17 9:27 PM
+ * @description
+ */
+public class WebTest {
+    
+    @Test
+    public void test() {
+        System.out.println("webTest");
+    }
+}
